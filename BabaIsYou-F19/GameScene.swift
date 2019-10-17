80f89aa771af8a8531820de1284d475b5a8a4912
akshdeepkaur2 <39845021+akshdeepkaur2@users.noreply.github.com>
2019-10-17 11:54:01 -0400

    R1- implement baba

--- BabaIsYou-F19/GameScene.swift
+++ BabaIsYou-F19/GameScene.swift
@@ -11,6 +11,22 @@ import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
 var baba = [SKSpriteNode]()
+    func createbaba() -> SKSpriteNode{
+        let baba = SKSpriteNode(imageNamed: "rabbit64")
+        baba.anchorPoint = CGPoint(x: 0, y: 0)
+        baba.size = CGSize(width: 80, height: 80)
+        baba.position = CGPoint(x: 535, y: 1135.618)
+        baba.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+        baba.physicsBody?.affectedByGravity = true
+        baba.physicsBody?.isDynamic = true
+        baba.physicsBody?.categoryBitMask = 16
+        baba.physicsBody?.collisionBitMask = 100
+        baba.physicsBody?.contactTestBitMask = 28
+        baba.physicsBody?.restitution = 0
+        baba.physicsBody?.allowsRotation = false
+        baba.name =  "rabbit64"
+        return baba
+    }
     override func didMove(to view: SKView) {
         self.physicsWorld.contactDelegate = self
     }
